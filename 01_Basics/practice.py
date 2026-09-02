a605d13079cf20ec9e3f9ab41be476627c1b2c50
Chandan Yadav <codesbychandan@gmail.com>
2026-09-02 18:30:28 +0530

    Create practice.py
    Added basic Python practice questions

---
+++ 01_Basics/practice.py
@@ -0,0 +1,30 @@
+# ==========================================
+# PYTHON BASICS - PRACTICE
+# ==========================================
+
+
+# Q1. Print your name
+
+name = input("Enter your name: ")
+print(name)
+
+
+# ==========================================
+# Q2. Add two numbers
+# ==========================================
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print(a + b)
+
+
+# ==========================================
+# Q3. Calculate area of a circle
+# ==========================================
+
+radius = float(input("Enter radius: "))
+
+area = 3.14 * radius * radius
+
+print("Area =", area)
